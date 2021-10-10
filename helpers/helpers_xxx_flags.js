5e0f47ce17780d1b880eb6a33314543caeb4e39c
regorxxx <regorxxx@protonmail.com>
2021-10-10 20:05:02 +0200

    Update helpers_xxx_flags.js

---
+++ helpers/helpers_xxx_flags.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+//07/10/21
+
+const KMask = {
+	none: 0,
+	ctrl: 1,
+	shift: 2,
+	ctrlshift: 3,
+	ctrlalt: 4,
+	ctrlaltshift: 5,
+	alt: 6
+};
+
+const ButtonStates = {
+	normal: 0,
+	hover: 1,
+	down: 2,
+	hide: 3
+};
+
+function GetKeyboardMask() {
+	var c = utils.IsKeyPressed(VK_CONTROL) ? true : false;
+	var a = utils.IsKeyPressed(VK_ALT) ? true : false;
+	var s = utils.IsKeyPressed(VK_SHIFT) ? true : false;
+	var ret = KMask.none;
+	if (c && !a && !s)
+		ret = KMask.ctrl;
+	if (!c && !a && s)
+		ret = KMask.shift;
+	if (c && !a && s)
+		ret = KMask.ctrlshift;
+	if (c && a && !s)
+		ret = KMask.ctrlalt;
+	if (c && a && s)
+		ret = KMask.ctrlaltshift;
+	if (!c && a && !s)
+		ret = KMask.alt;
+	return ret;
+};
