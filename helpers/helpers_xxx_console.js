fc0b0b6fff62824a542982f345f72cd2c21e68cc
regorxxx <regorxxx@protonmail.com>
2022-06-22 20:48:48 +0200

    Console: https://github.com/regorxxx/Console-SMP/commit/f670b9de3daa1e02a29b96e0403abd09d1d3f362

--- helpers/helpers_xxx_console.js
+++ helpers/helpers_xxx_console.js
@@ -1,11 +1,15 @@
 ﻿'use strict';
-//05/06/22
+//19/06/22
 
 include(fb.ComponentPath + 'docs\\Codepages.js');
 
 // Console log file
 let conLog = fb.ProfilePath + 'console.log'; // Edit here to change logging file. Replace with '' or null to disable logging
 let conLogMaxSize = 5000000; // File size, in bytes. Setting to zero or null disables logging too
+let conLogThrottling = 100; // Interval to flush to console, in ms. Setting to zero or null writes to console file on every call (not recommended)
+
+let conLogTimer; // Interval use.
+let conLogCache = []; // Internal use.
 
 // Override logging
 function consoleLog() {
@@ -17,37 +21,40 @@ function consoleLog() {
 	const stamp = '[' + new Date().toLocaleTimeString() + ']';
 	log += (log && log.length ? '\n' : '') + stamp;
 	// Unpack args
-	[...arguments].forEach((arg) => {
-		const type = typeof arg;
-		let val = null;
-		switch (type) {
-			case 'undefined': {
-				val = void(0);
-				break;
-			}
-			case 'function':
-			case 'number':
-			case 'boolean':
-			case 'string': {
-				val = arg.toString();
-				break;
-			}
-			case 'object':
-			default : {
-				let instance = null;
-				switch (true) {	// Get object types
-					case arg instanceof Set: {instance = 'Set '; break;}
-					case arg instanceof Map: {instance = 'Map '; break;}
-					case arg instanceof WeakMap: {instance = 'WeakMap '; break;}
-					case arg instanceof WeakSet: {instance = 'WeakSet '; break;}
+	const args = conLogCache.length ? conLogCache : [[...arguments]];
+	args.forEach((call) => {
+		call.forEach((arg) => {
+			const type = typeof arg;
+			let val = null;
+			switch (type) {
+				case 'undefined': {
+					val = void(0);
+					break;
 				}
-				if (instance) {val = Array.from(arg);} // Convert to array objects if possible and stringify
-				try {val = (instance ? instance : 'Object ') + JSON.stringify(val ? val : arg);} 
-				catch (e) {val = '--error type--'; console.logUI('Console.log: argument type not recognized: ', type, val ? val : arg);}
-				break;
+				case 'function':
+				case 'number':
+				case 'boolean':
+				case 'string': {
+					val = arg.toString();
+					break;
+				}
+				case 'object':
+				default : {
+					let instance = null;
+					switch (true) {	// Get object types
+						case arg instanceof Set: {instance = 'Set '; break;}
+						case arg instanceof Map: {instance = 'Map '; break;}
+						case arg instanceof WeakMap: {instance = 'WeakMap '; break;}
+						case arg instanceof WeakSet: {instance = 'WeakSet '; break;}
+					}
+					if (instance) {val = Array.from(arg);} // Convert to array objects if possible and stringify
+					try {val = (instance ? instance : 'Object ') + JSON.stringify(val ? val : arg);} 
+					catch (e) {val = '--error type--'; console.logUI('Console.log: argument type not recognized: ', type, val ? val : arg);}
+					break;
+				}
 			}
-		}
-		log += ' ' + val;
+			log += ' ' + val;
+		});
 	});
 	// Write
 	try {utils.WriteTextFile(conLog, log, false);} catch (e) {/* continue regardless of error */}
@@ -75,7 +82,42 @@ if (conLog && conLog.length && conLogMaxSize && console.log) {
 	console.logUI = console.log;
 	console.log = function() {
 		console.logUI(...arguments);
-		consoleLog(...arguments);
+		if (conLogThrottling) {
+			clearTimeout(conLogTimer);
+			conLogCache.push([...arguments]);
+			conLogTimer = setTimeout(consoleLog, conLogThrottling);
+		} else {
+			consoleLog(...arguments);
+		}
+	};
+	console.checkSize();
+}
+
+// Rewrap FbProfiler to expose Name variable
+if (FbProfiler.prototype) {
+	const oldProto = FbProfiler.prototype;
+	const oldFunc = FbProfiler;
+	FbProfiler = function(name) {
+		const obj = oldFunc(name);
+		obj.Name = name;
+		return obj;
+	};
+	FbProfiler.prototype = oldProto;
+}
+
+// Rewrap FbProfiler to also log to file
+if (FbProfiler.prototype.Print) {
+	FbProfiler.prototype.PrintUI = FbProfiler.prototype.Print;
+	FbProfiler.prototype.Print = function(additionalMsgopt = '', printComponentInfoopt = true) {
+		// Recreate the message format
+		let message = '';
+		if (printComponentInfoopt) {message += 'Spider Monkey Panel v' + utils.Version + ': ';}
+		message += 'profiler (' + this.Name + '): ';
+		if (additionalMsgopt && additionalMsgopt.length) {message += additionalMsgopt + ' ';}
+		message += this.Time + 'ms';
+		console.log(message); // Instead of using the original method, just use the basic log routine with debounce
+		// this.PrintUI(additionalMsgopt, printComponentInfoopt);
+		// consoleLog(message);
 	};
 	console.checkSize();
 }
