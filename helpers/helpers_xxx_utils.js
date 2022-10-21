2433be9ac3ad9ec290e7c8a8f8cc8c8cf3ec0d2d
regorxxx <regorxxx@protonmail.com>
2022-10-21 14:41:17 +0200

    - Track Auto-tagging: added new JS functions to retrieve properties from playlist: ('JS:' +) playlistName, playlistCategory, playlistTags and playlistMBID. - Track Auto-tagging: reworked entire feature to fine-tune overwriting or value addition behavior, in particular for JS functions. Empty values will now delete the tag, otherwise added to current tag by default. Check documentation for further details.

--- helpers/helpers_xxx_utils.js
+++ helpers/helpers_xxx_utils.js
@@ -1,8 +1,12 @@
 ﻿'use strict';
-//19/10/21
+//21/10/21
 
 /*
   Meant to be called by name on json files or other external tools to limit eval usage.
+  Playlist Manager:
+	- bOverwrite: when is true, new value will replace the current tag in any case.
+	- bMultiple: when is true, new value will be added to the current tag values (no overwriting it).
+	- Both false: new value is only added if tag has not been set before.
 */
 
 const funcDict = {
@@ -10,23 +14,30 @@ const funcDict = {
 		let today = new Date();
 		let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 		let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-		return date + ' ' + time;
+		return {value: date + ' ' + time, bOverWrite: false, bMultiple: false};
 	},
 	playlistName: function playlistName(pls) {
-		return pls.name;
+		return {value: [pls.name], bOverWrite: true, bMultiple: false};
 	},
 	playlistCategory: function playlistCategory(pls) {
-		return pls.category;
+		return {value: [pls.category], bOverWrite: true, bMultiple: false};
 	},
 	playlistTags: function playlistTags(pls) {
-		return pls.tags.join(',');
+		return {value: pls.tags.slice(), bOverWrite: true, bMultiple: false};
 	},
 	playlistMBID: function playlistMBID(pls) {
-		return pls.playlist_mbid;
+		return {value: [pls.playlist_mbid], bOverWrite: true, bMultiple: false};
+	},
+	playlistNameMult: function playlistNameMult(pls) {
+		return {value: [pls.name], bOverWrite: false, bMultiple: true};
+	},
+	playlistCategoryMult: function playlistCategoryMult(pls) {
+		return {value: [pls.category], bOverWrite: false, bMultiple: true};
+	},
+	playlistTagsMult: function playlistTagsMult(pls) {
+		return {value: pls.tags.slice(), bOverWrite: false, bMultiple: true};
+	},
+	playlistMBIDMult: function playlistMBIDMult(pls) {
+		return {value: [pls.playlist_mbid], bOverWrite: false, bMultiple: true};
 	}
 };
-
-
-
-
-
