ca1e08b1d05beb248792727bb6da084ad8d0b7ab
regorxxx <regorxxx@protonmail.com>
2023-07-29 15:12:25 +0200

    - Configuration: some fixes for ALBUM ARTIST usage instead of ARTIST.  To apply the change on existing installations, delete '[foobar_profile]\js_data\presets\global\globQuery.json' and '[foobar_profile]\foobar2000\js_data\presets\global\globTags.json' files. Also click on settings menut at 'Playlist Behavior\Export and Convert\Restore defaults'.

--- playlist_manager.js
+++ playlist_manager.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-//25/07/23
+//29/07/23
 
 /* 	Playlist Manager
 	Manager for Playlists Files and Auto-Playlists. Shows a virtual list of all playlists files within a configured folder (playlistPath).
@@ -104,11 +104,11 @@ var properties = {
 	converterPreset			: ['Converter Preset list', JSON.stringify([
 		{name: '', dsp: '...', tf: '.\\%FILENAME%.mp3', path: '', extension: ''}, // Export all at same folder
 		{name: '', dsp: '...', tf: '.\\' + _t(globTags.artist) + '\\%ALBUM%\\%TRACK% - %TITLE%.mp3', path: '', extension: ''}, // Transfer library
-		{name: '--Kodi Librelec (<your_disk_name>)--', dsp: '...', tf: '/media/<your_disk_name>/music/$puts(art,$ascii($meta(' + globTags.artist + ',0)))$ifequal($strrchr($get(art),.),$len($get(art)),$puts(art,$cut($get(art),$sub($len($get(art)),1))),)$puts(alb,$ascii(%album%))$ifequal($strrchr($get(alb),.),$len($get(alb)),$puts(alb,$cut($get(alb),$sub($len($get(alb)),1))),)$replace($get(art),:,-,/,-,?,)/$replace($get(alb),:,-,/,-,?,)/$replace($ascii(%TRACK% - %TITLE%),:,-,/,-,?,).lossy.flac', path: '', extension: '.m3u'}, // Kodi-like library
-		{name: '--Kodi Windows (<your_disk_name>)--', dsp: '...', tf: '<your_disk_name>:\\music\\$ascii($meta(' + globTags.artist + ',0)\\%album%\\%TRACK% - %TITLE%).mp3', path: '', extension: '.m3u'}, // Kodi-like library
-		{name: '--Foobar2000 mobile (playlists folder)--', dsp: '...', tf: '..\\music\\$ascii($meta(' + globTags.artist + ',0)\\%ALBUM%\\%TRACK% - %TITLE%).mp3', path: '', extension: '.m3u8'}, // Foobar2000 mobile, playlists on different folder than music
-		{name: '--Foobar2000 mobile (root)--', dsp: '...', tf: '.\\music\\$ascii($meta(' + globTags.artist + ',0)\\%ALBUM%\\%TRACK% - %TITLE%).mp3', path: '', extension: '.m3u8'}, // Foobar2000 mobile, playlists on same root than music (without a folder)
-		{name: '--Foobar2000 mobile (same folder)--', dsp: '...', tf: '.\\$ascii($meta(' + globTags.artist + ',0)\\%ALBUM%\\%TRACK% - %TITLE%).mp3', path: '', extension: '.m3u8'} // Foobar2000 mobile, playlists on same folder than music
+		{name: '--Kodi Librelec (<your_disk_name>)--', dsp: '...', tf: '/media/<your_disk_name>/music/$puts(art,$ascii($if2($meta(' + globTags.artistRaw + ',0),$meta(ARTIST,0))))$ifequal($strrchr($get(art),.),$len($get(art)),$puts(art,$cut($get(art),$sub($len($get(art)),1))),)$puts(alb,$ascii(%album%))$ifequal($strrchr($get(alb),.),$len($get(alb)),$puts(alb,$cut($get(alb),$sub($len($get(alb)),1))),)$replace($get(art),:,-,/,-,?,)/$replace($get(alb),:,-,/,-,?,)/$replace($ascii(%TRACK% - %TITLE%),:,-,/,-,?,).lossy.flac', path: '', extension: '.m3u'}, // Kodi-like library
+		{name: '--Kodi Windows (<your_disk_name>)--', dsp: '...', tf: '<your_disk_name>:\\music\\$ascii($if2($meta(' + globTags.artistRaw + ',0),$meta(ARTIST,0))\\%album%\\%TRACK% - %TITLE%).mp3', path: '', extension: '.m3u'}, // Kodi-like library
+		{name: '--Foobar2000 mobile (playlists folder)--', dsp: '...', tf: '..\\music\\$ascii($if2($meta(' + globTags.artistRaw + ',0),$meta(ARTIST,0))\\%ALBUM%\\%TRACK% - %TITLE%).mp3', path: '', extension: '.m3u8'}, // Foobar2000 mobile, playlists on different folder than music
+		{name: '--Foobar2000 mobile (root)--', dsp: '...', tf: '.\\music\\$ascii($if2($meta(' + globTags.artistRaw + ',0),$meta(ARTIST,0))\\%ALBUM%\\%TRACK% - %TITLE%).mp3', path: '', extension: '.m3u8'}, // Foobar2000 mobile, playlists on same root than music (without a folder)
+		{name: '--Foobar2000 mobile (same folder)--', dsp: '...', tf: '.\\$ascii($if2($meta(' + globTags.artistRaw + ',0),$meta(ARTIST,0))\\%ALBUM%\\%TRACK% - %TITLE%).mp3', path: '', extension: '.m3u8'} // Foobar2000 mobile, playlists on same folder than music
 	])],
 	bForbidDuplicates		: ['Skip duplicates when adding to playlists', true, {func: isBoolean}, true],
 	bDeadCheckAutoSave		: ['Warn about dead items on auto-save', false, {func: isBoolean}, false],
