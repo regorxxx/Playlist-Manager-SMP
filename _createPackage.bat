2d2db594d1afe5684ddf15308b90eb6c40c9f8bd
regorxxx <regorxxx@protonmail.com>
2023-11-09 23:51:28 +0100

    Update package builder

--- _createPackage.bat
+++ _createPackage.bat
@@ -77,6 +77,9 @@ CALL :copy_folder main\world_map
 CALL :check_folder main\statistics
 CALL :copy_file main\statistics\statistics_xxx.js
 CALL :copy_file main\statistics\statistics_xxx_helper.js
+CALL :check_folder main\window
+CALL :copy_file main\window\window_xxx_button.js
+CALL :copy_file main\window\window_xxx_helpers.js
 REM helpers
 CALL :check_folder helpers
 CALL :copy_file helpers\callbacks_xxx.js
@@ -119,6 +122,7 @@ CALL :delete_file helpers-external\countries-mercator\"_N. Cyprus.png"
 CALL :delete_file helpers-external\countries-mercator\_Somaliland.png
 CALL :delete_file helpers-external\countries-mercator\worldmap_natural.png
 CALL :delete_file helpers-external\countries-mercator\worldmap_shapes.png
+CALL :copy_folder helpers-external\natsort
 REM others
 CALL :check_folder _images
 CALL :copy_file _images\_Installation_v1.5_v1.4.JPG
@@ -173,6 +177,7 @@ CALL :check_folder main\filter_and_query
 CALL :copy_file main\filter_and_query\remove_duplicates.js
 CALL :copy_folder main\playlist_manager
 CALL :check_folder main\window
+CALL :copy_file main\window\window_xxx_button.js
 CALL :copy_file main\window\window_xxx_helpers.js
 CALL :copy_file main\window\window_xxx_input.js
 CALL :copy_file main\window\window_xxx_scrollbar.js
@@ -238,6 +243,7 @@ CALL :delete_file helpers-external\chroma.js\chroma-ultra-light.min.js
 CALL :copy_folder helpers-external\cmdutils
 CALL :copy_folder helpers-external\fuse
 CALL :copy_folder helpers-external\keycode-2.2.0
+CALL :copy_folder helpers-external\natsort
 CALL :copy_folder helpers-external\SimpleCrypto-js
 CALL :copy_folder helpers-external\xspf-to-jspf-parser
 CALL :copy_folder helpers-external\xsp-to-jsp-parser
@@ -348,6 +354,11 @@ CALL :copy_folder main\timeline
 CALL :copy_folder main\statistics
 CALL :check_folder main\filter_and_query
 CALL :copy_file main\filter_and_query\remove_duplicates.js
+CALL :check_folder main\window
+CALL :copy_file main\window\window_xxx_button.js
+CALL :copy_file main\window\window_xxx_background.js
+CALL :copy_file main\window\window_xxx_background_menu.js
+CALL :copy_file main\window\window_xxx_helpers.js
 REM helpers
 CALL :check_folder helpers
 CALL :copy_file helpers\callbacks_xxx.js
@@ -379,12 +390,6 @@ CALL :copy_folder helpers-external\bitmasksorterjs
 CALL :copy_folder helpers-external\natsort
 CALL :copy_folder helpers-external\chroma.js
 CALL :delete_file helpers-external\chroma.js\chroma-ultra-light.min.js
-REM helpers external
-CALL :check_folder main\window
-CALL :copy_file main\window\window_xxx_button.js
-CALL :copy_file main\window\window_xxx_background.js
-CALL :copy_file main\window\window_xxx_background_menu.js
-CALL :copy_file main\window\window_xxx_helpers.js
 REM package info, zip and report
 CALL :create_package_info
 CALL :compress %name% %version%
