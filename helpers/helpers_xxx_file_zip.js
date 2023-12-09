d84783b63e41a3189a6198ebc884363e21e6a85b
regorxxx <regorxxx@protonmail.com>
2023-12-09 22:35:48 +0100

    - Helpers: updated helpers.

--- helpers/helpers_xxx_file_zip.js
+++ helpers/helpers_xxx_file_zip.js
@@ -1,15 +1,24 @@
 ﻿'use strict';
-//27/11/23
+//09/12/23
 
 include('helpers_xxx.js');
 include('helpers_xxx_file.js');
 
 // _zip(folders.xxx + 'test.txt','test.zip');
 // _zip(['test.txt', 'test2.txt'], 'test.zip');
-function _zip(file, toPath, bAsync = false, relativePath = null) {
+function _zip(file, toPath, bAsync = false, relativePath = null, timeout = 0) {
 	const cmd = _q(folders.xxx + 'helpers-external\\7z\\' + (soFeat.x64 ? '7za' : '7za_32')+ '.exe') + ' a -tzip ' + _q(toPath) + ' ' + (isArrayStrings(file) ? file.map((f) => {return _q(f);}).join(' ') : _q(file));
-	const relCmd = relativePath ? 'CMD /Q /C ' + _q('CD ' + _q(relativePath) + ' && ' +  cmd) : null;
-	_runCmd(relCmd || cmd, !bAsync);
+	let relCmd;
+	if (timeout) {
+		if (relativePath) {
+			relCmd = 'CMD /Q /C' + _q('CD ' + _q(relativePath) + ' && TIMEOUT /T ' + timeout+ ' /NOBREAK >nul && ' +  cmd);
+		} else {
+			relCmd = 'CMD /Q /C ' + _q('TIMEOUT /T ' + timeout+ ' /NOBREAK >nul && ' +  cmd);
+		}
+	} else if (relativePath) {
+		relCmd = 'CMD /Q /C ' + _q('CD ' + _q(relativePath) + ' && ' +  cmd);
+	}
+	_runCmd(relCmd || cmd, !(bAsync || timeout));
 }
 
 // _unzip(folders.xxx + 'test.zip', folders.xxx + 'test\\');
