d7433889d804fac00163fa3291edc7353d0c5425
regorxxx <regorxxx@protonmail.com>
2023-12-13 17:31:37 +0100

    Update package builder

--- _createPackage.bat
+++ _createPackage.bat
@@ -1,6 +1,6 @@
 @ECHO off
 REM ------------------------------------------------------------------
-REM Create packages (zip file) from js files v.12/12/23
+REM Create packages (zip file) from js files v.13/12/23
 REM Requires 7za.exe on windows to compress (otherwise do it manually)
 REM If it's not provided, can be downloaded from:
 REM 	https://www.7-zip.org/download.html
@@ -9,15 +9,24 @@ REM 		Move '7za.exe' to 'C:\Windows\' (or equivalent path)
 REM 		Alternatively set path at zipExec variable
 REM Bat file must be placed at the root of the js files
 REM Usage:
-REM 	_createPackage.bat [Number]
+REM 	_createPackage.bat [Number] [foobarPath]
 REM Key:
 REM 	Number		If Number is provided, will build package without
 REM					user input
+REM 	foobarPath	If provided, will copy the package to the SMP
+REM					packages folder at profile, and run foobar2000
+REM					If it's already running, is qracefully quit first
+REM					If set to '.', will search for the binary file
+REM					relative to the current folder (4 folders up)
 REM ------------------------------------------------------------------
 SETLOCAL
 SET packagesFolder=packages
 SET zipExec=helpers-external\7z\7za_32.exe
 SET hasErrors=false
+SET foobarPath=%2
+IF [%foobarPath%]==[.] (
+	SET foobarPath="..\..\..\..\foobar2000.exe"
+)
 ECHO ---------------------------------
 ECHO ^|	Package creator:	^|
 ECHO ---------------------------------
@@ -26,7 +35,7 @@ ECHO (2) Playlist-Manager-SMP
 ECHO (3) Not-a-Waveform-Seekbar-SMP
 ECHO (4) Timeline-SMP
 ECHO.
-IF [%1]==[] (
+IF [%~1]==[] (
 	CHOICE /C 1234 /N /M "CHOOSE PACKAGE TO BUILD (1-4): "
 ) ELSE (
 	IF [%1] EQU [0] (
@@ -57,7 +66,7 @@ SET id=FA5A85D5-5C81-4B9B-BF01-52872BA83EA7
 SET description=https://regorxxx.github.io/foobar2000-SMP.github.io/scripts/world-map-smp/\r\n\r\nA foobar2000 UI Spider Monkey Panel which displays current artist's country on the world map and lets you generate autoplaylists based on selection and locale tag saving when integrated along WilB's Biography Script.\r\n\r\n• Map image configurable:\r\n   - Full.\r\n   - No Antarctica.\r\n   - Custom. (coordinates may need a transformation to work)\r\n• Configurable X and Y factors for transformation (along custom image maps).\r\n• 2 modes:\r\n   - Standard: Follow now playing track or selection.\r\n   - Library: display statistics of entire library (independtly of the selection/playback).\r\n• Works with multiple selected tracks (draws all points on the map), allowing to show statistics of an entire playlist or library.\r\n• Fully configurable UI.\r\n• On playback the panel fetches tags from (by order of preference):\r\n   - Track's tags.\r\n   - JSON database.\r\n   - WilB's Biography panel.\r\n• WilB's Biography integration\r\n• Tool-tip shows multiple info about the points and tracks selected.\r\n• AutoPlaylist creation on click over a point with any artist on your library from the selected country.\r\n• Fully Wine - Unix - non IE SOs compatible.
 REM version
 FOR /F "tokens=* USEBACKQ" %%F IN (`findstr /R "version:" world_map.js`) DO (SET version=%%F)
-IF "%version%" == "" (
+IF "%version%"=="" (
 	ECHO Main file not found or wrong version string
 	PAUSE>NUL
 	EXIT /B 1
@@ -149,10 +158,15 @@ CALL :copy_folder images\flags\32 true
 CALL :copy_folder images\flags\64 true
 CALL :copy_file images\MC_WorldMap.jpg
 CALL :copy_file images\MC_WorldMap_No_Ant.jpg
-CALL :copy_file images\MC_WorldMap.png
-CALL :copy_file images\MC_WorldMap_No_Ant.png
 CALL :copy_file images\MC_WorldMap_Shapes.png
 CALL :copy_file images\MC_WorldMap_Shapes_No_Ant.png
+CALL :check_folder images\hires
+CALL :copy_file images\hires\MC_WorldMap.jpg
+CALL :copy_file images\hires\MC_WorldMap.png
+CALL :copy_file images\hires\MC_WorldMap_No_Ant.jpg
+CALL :copy_file images\hires\MC_WorldMap_No_Ant.png
+CALL :copy_file images\hires\MC_WorldMap_Shapes.png
+CALL :copy_file images\hires\MC_WorldMap_Shapes_No_Ant.png
 CALL :check_folder presets
 CALL :check_folder presets\AutoHotkey
 CALL :copy_file presets\AutoHotkey\foobar_preview_play.ahk
@@ -160,13 +174,7 @@ CALL :copy_file presets\AutoHotkey\foobar_preview_sel.ahk
 CALL :copy_file presets\AutoHotkey\readme.txt
 CALL :copy_folder presets\"World Map"
 REM package info, zip and report
-IF NOT %hasErrors% == true (
-	CALL :create_package_info
-	CALL :compress %name% %version%
-)
-CALL :clean_root
-CALL :report
-IF %hasErrors% == true (EXIT /B 1)
+CALL :finish
 GOTO:EOF
 
 :playlist_manager
@@ -178,7 +186,7 @@ SET id=2A6AEDC9-BAE4-4D30-88E2-EDE7225B494D
 SET description=https://regorxxx.github.io/foobar2000-SMP.github.io/scripts/playlist-manager-smp/\r\n\r\nPlaylist manager for foobar2000 and Spider Monkey Panel to save and load (auto)playlists on demand, synchronizing, ... along many more utilities.\r\n\r\n• Manages Playlist files, AutoPlaylists and Smart Playlists(XBMC or Kodi).\r\n• ListenBrainz integration: sync user's playlists, ...\r\n• Loads playlist files x100 times faster than standard foobar.\r\n• Multiple exporting options: compatible with Foobar2000 mobile, Kodi and XBMC systems, etc.\r\n• Group by categories, tags and inline searching.\r\n• Filters and Sorting.\r\n• Configurable UI.\r\n• Fully Wine - Unix - non IE SOs compatible.\r\n• Other scripts integration:\r\n   - Playlist-Tools-SMP\r\n   - ajquery-xxx\r\n   - SMP Dynamic menus
 REM version
 FOR /F "tokens=* USEBACKQ" %%F IN (`findstr /R "version:" playlist_manager.js`) DO (SET version=%%F)
-IF "%version%" == "" (
+IF "%version%"=="" (
 	ECHO Main file not found or wrong version string
 	PAUSE>NUL
 	EXIT /B 1
@@ -296,13 +304,7 @@ CALL :copy_file examples\playlist_codepages.zip
 CALL :check_folder presets
 CALL :copy_folder presets\Network
 REM package info, zip and report
-IF NOT %hasErrors% == true (
-	CALL :create_package_info
-	CALL :compress %name% %version%
-)
-CALL :clean_root
-CALL :report
-IF %hasErrors% == true (EXIT /B 1)
+CALL :finish
 GOTO:EOF
 
 :not_a_waveform_seekbar
@@ -314,7 +316,7 @@ SET id=293B12D8-CC8B-4D21-8883-1A29EAFC4074
 SET description=https://github.com/regorxxx/Not-A-Waveform-Seekbar-SMP\r\n\r\nA seekbar for foobar2000, using Spider Monkey Panel, audiowaveform or ffprobe. It's based on RMS, peak levels, the actual waveform or visualization presets.\r\n\r\n• Uses audiowaveform by default (included).\r\n• ffprobe can be used if desired. Download it and copy ffprobe.exe into 'helpers-external\\ffprobe'.\r\n• Visualizer mode to simply show an animation which changes according to BPM (if tag exists).\r\n• Fully configurable using the R. Click menu:\r\n   - Colors\r\n   - Waveform modes\r\n   - Analysis modes\r\n   - Animations\r\n   - Refresh rate (not recommended anything below 100 ms except on really modern CPUs)
 REM version
 FOR /F "tokens=* USEBACKQ" %%F IN (`findstr /R "version:" seekbar.js`) DO (SET version=%%F)
-IF "%version%" == "" (
+IF "%version%"=="" (
 	ECHO Main file not found or wrong version string
 	PAUSE>NUL
 	EXIT /B 1
@@ -364,13 +366,7 @@ CALL :copy_folder helpers-external\lz-utf8
 CALL :copy_folder helpers-external\namethatcolor
 CALL :delete_file helpers-external\chroma.js\chroma.min.js
 REM package info, zip and report
-IF NOT %hasErrors% == true (
-	CALL :create_package_info
-	CALL :compress %name% %version%
-)
-CALL :clean_root
-CALL :report
-IF %hasErrors% == true (EXIT /B 1)
+CALL :finish
 GOTO:EOF
 
 :timeline
@@ -382,7 +378,7 @@ SET id=EAEB88D1-44AC-4B13-8960-FB3AAD78828D
 SET description=https://github.com/regorxxx/Timeline-SMP\r\n\r\nA timeline for foobar2000, using Spider Monkey Panel, and Statistics-Framework-SMP (https://github.com/regorxxx/Statistics-Framework-SMP).\r\n\r\n• Draws date (X) and nº tracks (Y) per artist (Z) by default.\r\n• Contextual menu to create playlists clicking on any point.\r\n• Fully customizable data per axis with TitleFormat.\r\n• Asynchronous data calculations. \r\n• Point statistics.\r\n• Scroll with buttons and mouse dragging.\r\n• Zoom with mouse wheel.\r\n• Configurable background.\r\n• Highly configurable chart and data manipulation.
 REM version
 FOR /F "tokens=* USEBACKQ" %%F IN (`findstr /R "version:" timeline.js`) DO (SET version=%%F)
-IF "%version%" == "" (
+IF "%version%"=="" (
 	ECHO Main file not found or wrong version string
 	PAUSE>NUL
 	EXIT /B 1
@@ -443,13 +439,7 @@ CALL :copy_folder helpers-external\chroma.js
 CALL :copy_folder helpers-external\namethatcolor
 CALL :delete_file helpers-external\chroma.js\chroma-ultra-light.min.js
 REM package info, zip and report
-IF NOT %hasErrors% == true (
-	CALL :create_package_info
-	CALL :compress %name% %version%
-)
-CALL :clean_root
-CALL :report
-IF %hasErrors% == true (EXIT /B 1)
+CALL :finish
 GOTO:EOF
 
 REM ------------------------------
@@ -484,7 +474,7 @@ GOTO:EOF
 :copy_folder
 SET folderPath=%1
 IF NOT EXIST %root%\%folderPath% MD %root%\%folderPath%
-IF [%2] == [true] (
+IF [%2]==[true] (
 	ECHO %folderPath%
 	COPY /V /Y %folderPath% %root%\%folderPath%>nul
 ) ELSE (
@@ -558,27 +548,21 @@ IF EXIST %packagesFolder%\fileName DEL /Q /F  %packagesFolder%\fileName
 GOTO:EOF
 
 :clean_root
-IF ERRORLEVEL 1 (
-	EXIT /B 1
-) ELSE (
-	CALL :delete_folder %root%
-)
+CALL :delete_folder %root%
+ECHO Temp folder cleaned.
 GOTO:EOF
 
 :report
-ECHO.
-ECHO.
-IF %hasErrors% == true (
+IF %hasErrors%==true (
 	ECHO Package was not created, some errors were found.
 ) ELSE (
 	IF ERRORLEVEL 1 (
 		ECHO 7z.exe not found, you must manually create a zip from the folder.
 		ECHO Check folder at: %root%
 	) ELSE (
-		ECHO Done. Check zip file at: %packagesFolder%\%name%-%version:.=-%-package.zip
+		SET output="Done. Check zip file at: %packagesFolder%\%name%-%version:.=-%-package.zip"
 	)
 )
-PAUSE>NUL
 GOTO:EOF
 
 :report_error
@@ -587,4 +571,51 @@ ECHO ERROR
 ECHO Not found: %1
 SET hasErrors=true
 EXIT /B 1
+GOTO:EOF
+
+:test
+REM Kill process and start again
+CHCP 437 > nul
+POWERSHELL $Process=Get-Process foobar2000 ^| Where-Object {$_.Path -like '*%foobarPath%*'} ; $Process.CloseMainWindow()>nul
+IF %ERRORLEVEL% EQU 0 (
+	ECHO Closing foobar2000...
+	TIMEOUT /t 4 /nobreak>nul
+)
+SET packages=%foobarPath:foobar2000.exe=profile\foo_spider_monkey_panel\packages\%
+ECHO Copying files to package folder...
+REM COPY /V /Y %root% %packages%\{%id%}>nul
+ROBOCOPY %root% %packages%\{%id%} /S /E /PURGE /NJH /NFL /NC /NDL /NJS /MT:16
+IF ERRORLEVEL 8 (SET hasErrors=true)
+ECHO Initializing foobar2000...
+START "foobar2000" /D %foobarPath:foobar2000.exe=% /B %foobarPath%
+GOTO:EOF
+
+:runFoobar
+IF EXIST %foobarPath% (
+	CALL :test
+) ELSE (
+	ECHO foobar2000 binary does not exist: %foobarPath%
+	SET hasErrors=true
+)
+GOTO:EOF
+
+REM package info, zip and report
+:finish
+IF NOT %hasErrors%==true (
+	CALL :create_package_info
+	CALL :compress %name% %version%
+	CALL :report
+	IF NOT [%foobarPath%]==[] (
+		ECHO Testing...
+		call :runFoobar
+	)
+)
+CALL :clean_root
+IF NOT [%output%]==[] (
+	ECHO.
+	ECHO.
+	ECHO %output:"=%
+)
+PAUSE>NUL
+IF %hasErrors%==true (EXIT /B 1)
 GOTO:EOF
